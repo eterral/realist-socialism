80d3ad8c48fae2b9a9e71f8ff0ecd9250dab0b02
eterral <elliott@eterral.com>
2022-01-25 12:48:18 -0600

    still testing

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 if IS_PRODUCTION:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
