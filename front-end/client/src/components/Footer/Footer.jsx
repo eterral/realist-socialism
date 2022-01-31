dac04bb3ac7ec73f2575f1f1440477f047c95dc5
eterral <elliott@eterral.com>
2022-01-30 23:00:04 -0600

    styled footer

--- front-end/client/src/components/Footer/Footer.jsx
+++ front-end/client/src/components/Footer/Footer.jsx
@@ -3,8 +3,12 @@ import '../Footer/Footer.css'
 export default function Footer() {
     return (
         <div className='footer'>
-            <h1 className='footer-text'>Linkedin</h1>
-            <h1 className='footer-text'>Github</h1>
+            <div className='footer-left'>
+                <h1 className='footer-text'>GitHub</h1>
+            </div>
+            <div className='footer-right'>
+                <h1 className='footer-text'>LinkedIn</h1>
+            </div>
         </div>
     )
 }
