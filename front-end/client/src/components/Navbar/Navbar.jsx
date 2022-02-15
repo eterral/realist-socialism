f39ab1578fc7dc726eb8b9b6f898f9d8790454c8
eterral <elliott@eterral.com>
2022-02-14 21:55:09 -0600

    fixing 4k resolution

--- front-end/client/src/components/Navbar/Navbar.jsx
+++ front-end/client/src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ export default function Navbar({user, setUser}) {
     }
     return (
     <div className='navbar'>
+        <div className='nav-top'>
         <div className='title'>
             <Link style={{ textDecoration: 'none', color: 'black' }} to="/"><h1 className='title-text'>Realist Socialism</h1></Link>
         </div>
@@ -20,11 +21,14 @@ export default function Navbar({user, setUser}) {
                 <Link style={{ textDecoration: 'none', color: 'white' }} to="/users"><h1 className='signin-text'>Sign In/Sign Up</h1></Link>
             </div>
         }
+        </div>
+        <div className='nav-bottom'>
         <div className='films-nav'>
         <Link style={{ textDecoration: 'none', color: 'white' }} to='/films'><h1 className='films-nav-text'>Films</h1></Link>
         </div>
         <div className='posters-nav'>
             <Link style={{ textDecoration: 'none', color: 'black' }} to='/posters'><h1 className='posters-nav-text'>Posters</h1></Link>
+        </div>
         </div>
     </div>
     )
